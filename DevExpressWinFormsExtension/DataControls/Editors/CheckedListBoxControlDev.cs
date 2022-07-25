e4b61648268bc0541ec800bbab9bcd8e03602864
a.kulikov <kulikov.dev@gmail.com>
2022-07-25 20:10:33 +0500

    CheckedListBoxControl with hotkeys

---
+++ DevExpressWinFormsExtension/DataControls/Editors/CheckedListBoxControlDev.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DevExpressWinFormsExtension.DataControls.Editors
+{
+    /// <summary>
+    /// CheckedListBoxControl component
+    /// </summary>
+    /// <remarks> The extension for the standard component supports hotkeys for fast check/uncheck of items. Ctrl+A: check all, Ctrl+D: uncheck all, Ctrl+I: invert checking. </remarks>
+    [ToolboxItem(true)]
+    [ToolboxBitmap(typeof(CheckedListBoxControl))]
+    [Description("CheckedListBoxControl with hotkeys")]
+    public class CheckedListBoxControlDev : CheckedListBoxControl
+    {
+        /// <summary>
+        /// Event on KeyDown
+        /// </summary>
+        /// <param name="e"> Parameters </param>
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Control | Keys.D:
+                    UnCheckAll();
+                    break;
+                case Keys.Control | Keys.A:
+                    CheckAll();
+                    break;
+                case Keys.Control | Keys.I:
+                    {
+                        InvertSelection();
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Inveret selection
+        /// </summary>
+        private void InvertSelection()
+        {
+            BeginItemsCheck();
+            try
+            {
+                foreach (CheckedListBoxItem item in Items)
+                {
+                    item.CheckState = item.CheckState == CheckState.Checked ? CheckState.Unchecked : CheckState.Checked;
+                }
+            }
+            finally
+            {
+                EndItemsCheck();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## DevExpress WinForms extension kit
+  A small library to solve some troubles, which I met during my work with DevExpress.
+
+### List of components
+#### Editors
+* CheckedListBoxControlDev. The extension for the standard component supports hotkeys for fast check/uncheck of items. Ctrl+A: check all, Ctrl+D: uncheck all, Ctrl+I: invert checking.
+* ColorPickEditDev, RepositoryItemColorPickEdit. The extensions for the standard components allows to save custom user colors during the program, so each ColorPickEdit and RepositoryItemColorPickEdit will have the same, actual user colors.
+
+#### LookUpDev
