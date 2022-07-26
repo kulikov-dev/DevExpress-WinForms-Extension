e3e350282bb72c6b4ea7ac023a525e757f4ecea6
a.kulikov <kulikov.dev@gmail.com>
2022-07-26 22:31:58 +0500

    InputBox with possibility of using custom function for user input validation

---
+++ DevExpressWinFormsExtension/DataControls/Editors/InputBoxValidableDev.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DevExpressWinFormsExtension.DataControls.Editors
+{
+    partial class InputBoxValidableDev
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.editValue = new DevExpress.XtraEditors.TextEdit();
+            this.toolTipController = new DevExpress.Utils.ToolTipController(this.components);
+            this.labelText = new DevExpress.XtraEditors.LabelControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnOk = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.editValue.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // editValue
+            // 
+            this.editValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editValue.Location = new System.Drawing.Point(65, 8);
+            this.editValue.Name = "editValue";
+            this.editValue.Properties.Appearance.BackColor = System.Drawing.Color.MistyRose;
+            this.editValue.Properties.Appearance.Options.UseBackColor = true;
+            this.editValue.Size = new System.Drawing.Size(236, 20);
+            this.editValue.TabIndex = 14;
+            this.editValue.ToolTipController = this.toolTipController;
+            this.editValue.EditValueChanging += new DevExpress.XtraEditors.Controls.ChangingEventHandler(this.EditValueChanging);
+            // 
+            // toolTipController
+            // 
+            this.toolTipController.AutoPopDelay = 3000;
+            this.toolTipController.ToolTipLocation = DevExpress.Utils.ToolTipLocation.BottomCenter;
+            // 
+            // labelText
+            // 
+            this.labelText.Location = new System.Drawing.Point(7, 11);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(52, 13);
+            this.labelText.TabIndex = 0;
+            this.labelText.Text = "Значение:";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.panelControl1.Controls.Add(this.btnOk);
+            this.panelControl1.Controls.Add(this.btnCancel);
+            this.panelControl1.Controls.Add(this.editValue);
+            this.panelControl1.Controls.Add(this.labelText);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.MaximumSize = new System.Drawing.Size(600, 36);
+            this.panelControl1.MinimumSize = new System.Drawing.Size(306, 36);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(306, 36);
+            this.panelControl1.TabIndex = 3;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Enabled = false;
+            this.btnOk.Location = new System.Drawing.Point(145, 32);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 15;
+            this.btnOk.Tag = "1";
+            this.btnOk.Text = "OK";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(226, 32);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Tag = "2";
+            this.btnCancel.Text = "Cancel";
+            // 
+            // InputBoxValidableDev
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelControl1);
+            this.MaximumSize = new System.Drawing.Size(500, 36);
+            this.MinimumSize = new System.Drawing.Size(306, 36);
+            this.Name = "InputBoxValidableDev";
+            this.Size = new System.Drawing.Size(306, 36);
+            ((System.ComponentModel.ISupportInitialize)(this.editValue.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelText;
+        private DevExpress.XtraEditors.TextEdit editValue;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.Utils.ToolTipController toolTipController;
+        public DevExpress.XtraEditors.SimpleButton btnOk;
+        public DevExpress.XtraEditors.SimpleButton btnCancel;
+    }
+}
---
+++ DevExpressWinFormsExtension/DataControls/Editors/InputBoxValidableDev.cs
@@ -0,0 +1,164 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraEditors.Controls;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using DevExpressWinFormsExtension.Helpers;
+using System.Drawing;
+using DevExpress.XtraEditors;
+
+namespace DevExpressWinFormsExtension.DataControls.Editors
+{
+    /// <summary>
+    /// InputBox with possibility of using custom function for user input validation
+    /// </summary>
+    [ToolboxItem(true)]
+    [ToolboxBitmap(typeof(TextEdit))]
+    [Description("InputBox with possibility of using custom function for user input validation.")]
+    public partial class InputBoxValidableDev : UserControlDev, IValidable
+    {
+        /// <summary>
+        /// Custom function of user input validation
+        /// </summary>
+        private readonly Func<string, string> customInputValidationFunc;
+
+        /// <summary>
+        /// Parameterless constructor
+        /// </summary>
+        private InputBoxValidableDev()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor with parameters
+        /// </summary>
+        /// <param name="defaultValue"> Default value in the inputBox </param>
+        /// <param name="labelTitle"> Title for the label </param>
+        /// <param name="customInputValidationFunc"> Custom function of user input validation </param>
+        private InputBoxValidableDev(string defaultValue, string labelTitle, Func<string, string> customInputValidationFunc)
+            : this()
+        {
+            if (!string.IsNullOrWhiteSpace(labelTitle))
+            {
+                labelText.Text = labelTitle;
+                editValue.Location = new Point(labelText.Location.X + labelText.Size.Width + 5, editValue.Location.Y);
+                editValue.Size = new Size(Width - labelText.Size.Width - 20, editValue.Size.Height);
+            }
+
+            editValue.Text = defaultValue;
+            this.customInputValidationFunc = customInputValidationFunc;
+            SetControlState(ValidateData());
+        }
+
+        /// <summary>
+        /// User input value
+        /// </summary>
+        public string InputValue
+        {
+            get
+            {
+                return editValue.Text.Trim();
+            }
+        }
+
+        /// <summary>
+        /// Show InputBox
+        /// </summary>
+        /// <param name="controlCaption"> Control's title caption </param>
+        /// <returns> User input value </returns>
+        public static string Show(string controlCaption)
+        {
+            return Show(string.Empty, controlCaption, string.Empty);
+        }
+
+        /// <summary>
+        /// Show InputBox
+        /// </summary>
+        /// <param name="controlCaption"> Control's title caption </param>
+        /// <param name="defaultValue"> Default value in the inputBox </param>
+        /// <param name="labelTitle"> Title for the label </param>
+        /// <returns> User input value </returns>
+        public static string Show(string controlCaption, string defaultValue, string labelTitle)
+        {
+            var result = Show(controlCaption, labelTitle);
+            return string.IsNullOrWhiteSpace(result) ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Show InputBox
+        /// </summary>
+        /// <param name="controlCaption"> Control's title caption </param>
+        /// <param name="labelTitle"> Title for the label </param>
+        /// <param name="customInputValidationFunc"> Custom function of user input validation </param>
+        /// <param name="parentControl"> Parent control </param>
+        /// <returns> Dialog result </returns>
+        public static string Show(string controlCaption, string labelTitle, Func<string, string> customInputValidationFunc = null, Control parentControl = null)
+        {
+            using (var control = new InputBoxValidableDev(string.Empty, labelTitle, customInputValidationFunc))
+            {
+                var result = ControlBox.Show(parentControl, control, controlCaption, MessageBoxButtons.OKCancel, 0, canResize: false, control);
+                if (result == DialogResult.OK)
+                {
+                    return control.InputValue;
+                }
+
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Setup control's state, depends on validation result
+        /// </summary>
+        /// <param name="isValid"> Validation result </param>
+        public void SetControlState(bool isValid)
+        {
+            btnOk.Enabled = isValid;
+        }
+
+        /// <summary>
+        /// Control validation
+        /// </summary>
+        /// <returns> True, if valid </returns>
+        public bool ValidateData()
+        {
+            var hasError = false;
+            if (string.IsNullOrEmpty(InputValue))
+            {
+                editValue.ToolTipController.ShowHint("Input value can't be empty.", editValue, ToolTipLocation.BottomCenter);
+                hasError = true;
+            }
+            else
+            {
+                toolTipController.HideHint();
+            }
+
+            if (customInputValidationFunc != null)
+            {
+                var textMessage = customInputValidationFunc(InputValue);
+                if (!string.IsNullOrWhiteSpace(textMessage))
+                {
+                    editValue.ToolTipController.ShowHint(textMessage, editValue, ToolTipLocation.BottomCenter);
+                    hasError = true;
+                }
+                else
+                {
+                    toolTipController.HideHint();
+                }
+            }
+
+            editValue.BackColor = hasError ? SkinHelper.GetEditorWarningBackColor() : SkinHelper.GetEditorBackColor();
+            return !hasError;
+        }
+
+        /// <summary>
+        /// Event on input value changing
+        /// </summary>
+        /// <param name="sender"> Source </param>
+        /// <param name="e"> Parameters </param>
+        private void EditValueChanging(object sender, ChangingEventArgs e)
+        {
+            SetControlState(ValidateData());
+        }
+    }
+}
