c54a3b0fc798cd567dec90eff695f7863cc4fe05
kulikov-dev <kulikov.dev@gmail.com>
2022-10-30 10:23:23 +0300

    FormsHelper to find a shown forms with specific title

---
+++ DevExpressWinFormsExtension/Utils/FormHelper.cs
@@ -0,0 +1,54 @@
+﻿using DevExpressWinFormsExtension.DataControls.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DevExpressWinFormsExtension.Utils
+{
+    /// <summary>
+    /// Helper to work with forms
+    /// </summary>
+    public static class FormHelper
+    {
+        /// <summary>
+        /// Check if there are any shown form with a specific title
+        /// </summary>
+        /// <param name="title"> Title to compare </param>
+        /// <param name="tagData"> Tag data to compare </param>
+        /// <param name="needFocus"> Flag if need focus on opened form </param>
+        /// <returns> Flag if the form showed </returns>
+        public static bool IsFormShown(string title, object tagData = null, bool needFocus = true)
+        {
+            return GetShownForm(title, tagData, needFocus) != null;
+        }
+
+        /// <summary>
+        /// Get shown form with a specific title
+        /// </summary>
+        /// <param name="title"> Title to compare </param>
+        /// <param name="tagData"> Tag data to compare </param>
+        /// <param name="needFocus"> Flag if need focus on opened form </param>
+        /// <returns> Shown form </returns>
+        public static XtraFormDev GetShownForm(string title, object tagData = null, bool needFocus = true)
+        {
+            XtraFormDev openForm = null;
+
+            foreach (var form in Application.OpenForms)
+            {
+                if (form is XtraFormDev devForm && (devForm.Text == title) && (tagData == null || tagData == devForm.Tag))
+                {
+                    openForm = devForm;
+
+                    break;
+                }
+            }
+
+            if ((openForm != null) && needFocus)
+            {
+                openForm.Focus();
+            }
+
+            return openForm;
+        }
+    }
+}
